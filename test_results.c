879ff96f352765933f4959b29d4d5c8c70cbe269
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-28 16:59:26 +0000

    Fri, Jul 28, 2023, 5:59 PM +01:00

---
+++ test_results.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+const int N = 3;
+// collection is defined as an ARRAY
+float average(int lengh, int collection[]);
+
+
+int main (void)
+{
+    int scores [N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("score: ");
+    }
+
+    printf("Average: %f\n", average(N, scores));
+
+}
+
+float average(int lengh, int collection[])
+{
+    int summierung = 0;
+    for (int i = 0; i < lengh; i++)
+    {
+        summierung += collection[i];
+    }
+    return summierung / (float) lengh;
+}
