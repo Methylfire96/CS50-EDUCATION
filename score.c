4b75f942b997d1c7d56faf4adb83457fa0a59f8d
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-28 16:07:36 +0000

    Fri, Jul 28, 2023, 5:07 PM +01:00

--- score.c
+++ score.c
@@ -3,7 +3,13 @@
 
 int main (void)
 {
-    int scores [3];
+    int scores [3]
+    do
+    {
+        scores++
+    }
+    while(scores < 0);
+    return scores;
     scores [0] = get_int("score 1: ");
     scores [1] = get_int("score 2: ");
     scores [2] = get_int("score 3: ");
