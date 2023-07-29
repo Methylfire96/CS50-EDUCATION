d2be86739a16069b81c1bd561b1ea694ad1804f1
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-29 15:18:10 +0000

    Sat, Jul 29, 2023, 4:18 PM +01:00

--- array.c
+++ array.c
@@ -11,7 +11,7 @@ int main (void)
     while(lengh < 1);
 
     int prog[lengh];
-    for(int i = 0; i < lengh; i++)
+    for(int i = 1; i < lengh; i++)
     {
         prog[i] = 2 *(prog[i-1]);
         printf("%i\n", prog[i]);
