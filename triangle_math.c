5fc45d0ca3c34c789898b12405c8694d3c46c1bf
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-30 22:04:08 +0000

    Sun, Jul 30, 2023, 11:04 PM +01:00

--- triangle_math.c
+++ triangle_math.c
@@ -2,28 +2,25 @@
 #include<stdio.h>
 
 
-
+bool triangle(float x, float y, float z);
 
 int main(void)
 {
-    float x = get_int("x: \n");
-    float y = get_int("y: \n");
-    float z = get_int("z: \n");
+    float x = get_int("x: ");
+    float y = get_int("y: ");
+    float z = get_int("z: ");
 
-    bool triangle(float x, float y, float z);
-}
-
-
-bool triangle(float x, float y, float z)
-{
-    if((x+y<=z) || (x+z<=y) || (y+z<=x))
+    bool triangle(float x, float y, float z)
     {
-        return false;
-    }
+        if((x+y<=z) || (x+z<=y) || (y+z<=x))
+        {
+            return false;
+        }
 
-    if ((x <= 0) || (y <= 0) || (z <= 0))
-    {
-        return false;
+        if ((x <= 0) || (y <= 0) || (z <= 0))
+        {
+           return false;
+        }
+        return true;
     }
-    return true;
 }
