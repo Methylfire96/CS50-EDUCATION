9deb8a3492e4db0d60c95d273758110dc0f18184
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-30 20:54:19 +0000

    Sun, Jul 30, 2023, 9:54 PM +01:00

--- alpha.c
+++ alpha.c
@@ -1,14 +1,15 @@
 #include<cs50.h>
 #include<stdio.h>
+#include<string.h>
 
 int main (void)
 {
     string word = get_string("word: ");
-}
 
-string prog[word];
-for(int i = 0; i < word; i++)
-{
+    int word_length = strlen(word);
+
+    for(int i = 0; i < word; i++)
+
     prog[i] = word[0] < word[1];
     printf("%s\n", prog[i])
 }
