b210bc14827f75f3c8571bfb6297c9bea4ea1928
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-31 22:47:03 +0000

    Mon, Jul 31, 2023, 11:47 PM +01:00

--- half/half.c
+++ half/half.c
@@ -15,10 +15,10 @@ int main(void)
     printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
 }
 
-// TODO: Complete the function
+
 float half(float bill, float tax, int tip)
 {
-    //float result = ( bill + (tax/100) + (tip/100)) /2;
+
     float total_bill = bill + (bill *(tax/100.0));
 
     float tip_amount = total_bill * (tip/100.0);
