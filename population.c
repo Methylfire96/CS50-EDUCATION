be1ec0f5d59a71354b0bc2f40f8dcf4fbdb02648
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-31 22:52:02 +0000

    Mon, Jul 31, 2023, 11:52 PM +01:00

---
+++ population.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    float start;
+    do
+    {
+        start= get_float("Start Size: ");
+    }
+    while (start<9);
+
+    float end;
+    do
+    {
+        end= get_float("End Size: ");
+    }
+    while (end<start);
+
+    float years = 0;
+    while (start < end)
+    {
+        start += start/12;
+        years++;
+    }
+    printf("Years: %f\n", years);
+}
