86378981311f76e270f295f1ab3894464831f4d5
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-07-31 17:37:35 +0000

    Mon, Jul 31, 2023, 6:37 PM +01:00

--- half/half.c
+++ half/half.c
@@ -18,7 +18,7 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float result = (float bill + (float tax/100) + (float tip/100))/2;
+    float result = ((float bill) + (float tax/100) + (float tip/100)) /2;
 
     return result;
 }
