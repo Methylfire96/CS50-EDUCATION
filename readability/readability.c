816dde8da99687e40b9397295901cee1c453d12f
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-03 23:27:50 +0000

    Fri, Aug 4, 2023, 12:27 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+int coleman_liau_formula(letters, words, sentences);
 
 int main(void)
 {
     string text = get_string("Please enter your text: ");
+
+    //counting letters, words, sentences
+
+    
 }
+
+
+
+int coleman_liau_formula(letters, words, sentences);
