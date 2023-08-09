7c29b6ee70ef90385ab0c3ed5bdfc2c88e6ab46b
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-09 23:14:00 +0000

    Thu, Aug 10, 2023, 12:14 AM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     {
         string plaintext = get_string("Plaintext: ");
         int c = count_c(plaintext);
-        printf("Ciphertext: %s, %s \n", plaintext, argv[]);
+        printf("Ciphertext: %s, %s, %i \n", plaintext, argv[], c);
         return 0;
     }
 
