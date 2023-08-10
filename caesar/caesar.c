41b3989fd79df22a5089c2ebca8cecafd918b0f3
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-10 00:18:59 +0000

    Thu, Aug 10, 2023, 1:18 AM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -25,10 +25,10 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < len; i++)
     {
-        if (isalpha(plaintext[i]))
+        if (isupper(plaintext[i]))
         {
-            char base = isupper(plaintext[i]) ? 'A' : 'a';
-            printf(" %c", (plaintext[i] + key) % 26);
+
+            printf(" %c", (plaintext[i] - 65 + key) % 26 + 65);
         }
         else
         {
