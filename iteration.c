bcce38f8bc7b7227d98130c53832770ada1cfb2b
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-21 17:20:15 +0000

    Mon, Aug 21, 2023, 6:20 PM +01:00

--- iteration.c
+++ iteration.c
@@ -17,7 +17,7 @@ void draw(int n)
 {
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j <= i; j++)
+        for(int j = 0; j < i+1; j++)
         {
             printf("#");
         }
