ec3ed758d8e36425fb0c59165a1804f7403d3d01
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-21 17:53:00 +0000

    Mon, Aug 21, 2023, 6:53 PM +01:00

---
+++ sorting_iter_recur/merging.c
@@ -0,0 +1,12 @@
+if only one number
+    quit
+else
+    soft left half of numbers
+    sort right half of numbers
+    merge sorted halves
+
+
+7/2/5/4/1/6/0/3
+2 7/4 5/1 6/0 3
+2 4 5 7/0 1 3 6
+0 1 2 3 4 5 6 7
