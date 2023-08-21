931de5e92bfa85c668452da934a6f2c637a08005
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-21 17:32:55 +0000

    Mon, Aug 21, 2023, 6:32 PM +01:00

--- recursion3.c
+++ recursion3.c
@@ -6,11 +6,15 @@ void draw(int n);
 
 int main(void)
 {
-    draw(1);
+    draw(3);
 }
 
 void draw(int n)
 {
+    if (n<= 0)
+    {
+        return;
+    }
     draw(n-1); // recursion2 macht erneut einen endlos loop!!
     for (int i = 0; i < n; i++)
     {
