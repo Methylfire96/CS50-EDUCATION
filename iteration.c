6b33e3f9775f0767fdd69b0eeb00714452c108d7
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-21 16:53:38 +0000

    Mon, Aug 21, 2023, 5:53 PM +01:00

---
+++ iteration.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    int height = get_int("Height: ");
+    draw(height);
+
+
+}
+
+void draw(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        
+    }
+}
