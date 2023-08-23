a0b7f84c475b8cbbb4d4ad62def0240e274b89eb
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-23 23:13:14 +0000

    Thu, Aug 24, 2023, 12:13 AM +01:00

--- lesson_test.c
+++ lesson_test.c
@@ -5,11 +5,11 @@ int main (void)
     char *s = "########";
     printf("%s %s\n", s,s);
     printf("%s   %s\n", s+1, s+1);
-    printf("%s    %s\n", s+2, s+2);
-    printf("%s     %s\n", s+3, s+3);
-    printf("%s      %s\n", s+4, s+4);
-    printf("%s     %s\n", s+3, s+3);
-    printf("%s    %s\n", s+2, s+2);
-    printf("%s   %s\n", s+1, s+1);
+    printf("%s     %s\n", s+2, s+2);
+    printf("%s       %s\n", s+3, s+3);
+    printf("%s         %s\n", s+4, s+4);
+    printf("%s       %s\n", s+3, s+3);
+    printf("%s      %s\n", s+2, s+2);
+    printf("%s    %s\n", s+1, s+1);
     printf("%s  %s\n", s, s);
 }
