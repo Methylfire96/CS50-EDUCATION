503cfae648caa4d44e4671a634ded3374ef0a5ad
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-23 23:14:14 +0000

    Thu, Aug 24, 2023, 12:14 AM +01:00

--- lesson_test.c
+++ lesson_test.c
@@ -9,7 +9,7 @@ int main (void)
     printf("%s       %s\n", s+3, s+3);
     printf("%s         %s\n", s+4, s+4);
     printf("%s       %s\n", s+3, s+3);
-    printf("%s      %s\n", s+2, s+2);
-    printf("%s    %s\n", s+1, s+1);
-    printf("%s  %s\n", s, s);
+    printf("%s     %s\n", s+2, s+2);
+    printf("%s   %s\n", s+1, s+1);
+    printf("%s %s\n", s, s);
 }
