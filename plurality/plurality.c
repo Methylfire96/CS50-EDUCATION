06be73b3f9c04d4ac22a6387838b3a911728eff1
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-23 18:11:18 +0000

    Wed, Aug 23, 2023, 7:11 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,7 +68,7 @@ bool vote(string name)
 {
     for(int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(name, candidates[i].name == 0))
+        if(strcmp(name, candidates[i].name))
         {
             candidates[i].votes++;
             return true;
@@ -83,12 +83,12 @@ void print_winner(void)
     int max_votes = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if(candidates[i].vote > max_votes)
+        if(candidates[i].votes > max_votes)
         {
             max_votes = candidates[i].votes;
         }
     }
-    for(int i = 0; i < cnadidate_count; i++)
+    for(int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes == max_votes)
         {
