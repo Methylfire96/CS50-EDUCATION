f3fb94284faeb55c2ffcd171bda59336cf4c5891
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-23 23:01:54 +0000

    Thu, Aug 24, 2023, 12:01 AM +01:00

---
+++ lesson_test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (void)
+{
+    char s = "#";
+    printf("%s\n", s)
+    printf("%s\n", s+1)
+    printf("%s\n", s+2)
+    printf("%s\n", s+3)
+    printf("%s\n", s+4)
+    printf("%s\n", s+3)
+    printf("%s\n", s+2)
+    printf("%s\n", s+1)
+
+}
