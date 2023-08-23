2657c43059a95c743fbb8bfe112eb5b28663ed41
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-23 23:05:15 +0000

    Thu, Aug 24, 2023, 12:05 AM +01:00

--- lesson_test.c
+++ lesson_test.c
@@ -2,10 +2,12 @@
 
 int main (void)
 {
-    char *s = "####";
+    char *s = "#####";
     printf("%s\n", s);
     printf("%s\n", s+1);
     printf("%s\n", s+2);
+    printf("%s\n", s+3);
+    printf("%s\n", s+4);
     printf("%s\n", s+3);
     printf("%s\n", s+2);
     printf("%s\n", s+1);
