ab134c904f006bfaa7d5c48bb16068420aa24fb6
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-24 23:15:17 +0000

    Fri, Aug 25, 2023, 12:15 AM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -30,6 +30,11 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaGreen = (int)((image[i][j].rgbtRed * .349) + (image[i][j].rgbtGreen * .686) + (image[i][j].rgbtBlue * .168)) + 0.5;
             int sepiaBlue = (int)((image[i][j].rgbtRed * .272) + (image[i][j].rgbtGreen * .534) + (image[i][j].rgbtBlue * .131)) + 0.5;
 
+            if(sepiaRed > 255 || sepiaGreen > 255 || sepiaBlue > 255)
+            {
+                sepiaRed =255, sepiaGreen = 255, sepiaBlue = 255;
+            }
+
             image[i][j].rgbtRed = sepiaRed;
             image[i][j].rgbtGreen = sepiaGreen;
             image[i][j].rgbtBlue = sepiaBlue;
