d1328b5861b56918f47837e18006cd52c8114671
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-24 20:18:29 +0000

    Thu, Aug 24, 2023, 9:18 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,6 +8,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for(int j = 0; j < width; j++)
         {
             if(image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtRed 
+            }
         }
     }
     return;
