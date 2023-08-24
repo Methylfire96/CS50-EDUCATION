750ea04f163a406666507b9a0a37074661b0112d
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-24 21:14:30 +0000

    Thu, Aug 24, 2023, 10:14 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,13 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height, i++)
+    for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
-            if(image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            if(image[i][j].rgbtRed < 0xFF /2 && image[i][j].rgbtGreen < 0xFF /2 && image[i][j].rgbtBlue < 0xFF /2)
             {
-                image[i][j].rgbtRed 
+                image[i][j].rgbtRed = 0x00, image[i][j].rgbtGreen = 0x00, image[i][j].rgbtBlue = 0x00;
             }
         }
     }
