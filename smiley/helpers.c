c33061f2a5e1b3ccf6322eb3ae17cb71f38fa40d
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-24 19:03:47 +0000

    Thu, Aug 24, 2023, 8:03 PM +01:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -8,7 +8,8 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         {
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
-                image[i][j].rgbGreen == 0xFF;
+                image[i][j].rgbtGreen = 0xFF;
+                break;
             }
         }
     }
