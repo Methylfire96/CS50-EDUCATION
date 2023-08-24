0361bfaf6b28e73f1e01f95bc44bcc9ff653e638
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-24 23:10:28 +0000

    Fri, Aug 25, 2023, 12:10 AM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,9 +26,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
-            int sepiaRed = ((image[i][j].rgbtRed * .393) + (image[i][j].rgbtGreen * .769) + (image[i][j].rgbtBlue * .189)) + 0.5;
-            int sepiaGreen = ((image[i][j].rgbtRed * .349) + (image[i][j].rgbtGreen * .686) + (image[i][j].rgbtBlue * .168)) + 0.5;
-            int sepiaBlue = ((image[i][j].rgbtRed * .272) + (image[i][j].rgbtGreen * .534) + (image[i][j].rgbtBlue * .131)) + 0.5;
+            int sepiaRed = (int)((image[i][j].rgbtRed * .393) + (image[i][j].rgbtGreen * .769) + (image[i][j].rgbtBlue * .189)) + 0.5;
+            int sepiaGreen = (int)((image[i][j].rgbtRed * .349) + (image[i][j].rgbtGreen * .686) + (image[i][j].rgbtBlue * .168)) + 0.5;
+            int sepiaBlue = (int)((image[i][j].rgbtRed * .272) + (image[i][j].rgbtGreen * .534) + (image[i][j].rgbtBlue * .131)) + 0.5;
 
             image[i][j].rgbtRed = sepiaRed;
             image[i][j].rgbtGreen = sepiaGreen;
