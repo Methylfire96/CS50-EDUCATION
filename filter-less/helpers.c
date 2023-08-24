55afcbd875041d6932dfb124531401a3ea98b511
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-24 21:21:52 +0000

    Thu, Aug 24, 2023, 10:21 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
-            if(image[i][j].rgbtRed < 0xFF /2 && image[i][j].rgbtGreen < 0xFF /2 && image[i][j].rgbtBlue < 0xFF /2)
+            if(image[i][j].rgbtRed == 0xFF /2 && image[i][j].rgbtGreen < 0xFF /2 && image[i][j].rgbtBlue < 0xFF /2)
             {
                 image[i][j].rgbtRed = 0x00, image[i][j].rgbtGreen = 0x00, image[i][j].rgbtBlue = 0x00;
             }
@@ -37,3 +37,15 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
 }
+
+
+/*{
+            if(image[i][j].rgbtRed < 0xFF /2 && image[i][j].rgbtGreen < 0xFF /2 && image[i][j].rgbtBlue < 0xFF /2)
+            {
+                image[i][j].rgbtRed = 0x00, image[i][j].rgbtGreen = 0x00, image[i][j].rgbtBlue = 0x00;
+            }
+            else
+            {
+                image[i][j].rgbtRed = 0xFF, image[i][j].rgbtGreen = 0xFF, image[i][j].rgbtBlue = 0xFF;
+            }
+        }*/
