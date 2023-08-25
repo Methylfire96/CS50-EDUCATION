37cd14ca13ec5911ce67ae58060a7a8a91193cf4
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-25 17:24:11 +0000

    Fri, Aug 25, 2023, 6:24 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -91,10 +91,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
                 }
             }
-            //math.h has the llround function, to get to the nearest number
-            temporary[i][j].rgbtRed = round(totalRed / counter);
-            temporary[i][j].rgbtGreen = round(totalGreen / counter);
-            temporary[i][j].rgbtBlue = round(totalBlue / counter);
+            //math.h has the LL round function, to get to the nearest number
+            temporary[i][j].rgbtRed = llround(totalRed / counter);
+            temporary[i][j].rgbtGreen = llround(totalGreen / counter);
+            temporary[i][j].rgbtBlue = llround(totalBlue / counter);
         }
     }
     for(int i = 0; i < height; i++)
