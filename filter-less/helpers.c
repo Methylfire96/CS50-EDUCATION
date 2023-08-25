5ec06bdf04ed27ce85313aeb5937511b32b47f59
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-25 17:56:50 +0000

    Fri, Aug 25, 2023, 6:56 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,21 +1,19 @@
 #include "helpers.h"
-#include<math.h>
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
-            int average = (image[i][j].rgbtRed + image[i][j].rgbtGreen +image[i][j].rgbtBlue )/3.0 + 0.5;
+            int average = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0 + 0.5;
 
             image[i][j].rgbtRed = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtBlue = average;
-
         }
-
     }
     return;
 }
@@ -23,9 +21,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {   // 255 is the max rgb number, but math.h needs fmin!!
             int sepiaRed = fmin(255, ((image[i][j].rgbtRed * .393) + (image[i][j].rgbtGreen * .769) + (image[i][j].rgbtBlue * .189))+ 0.5);
             int sepiaGreen = fmin(255, ((image[i][j].rgbtRed * .349) + (image[i][j].rgbtGreen * .686) + (image[i][j].rgbtBlue * .168))+ 0.5);
@@ -42,13 +40,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
-    {   //swap in the middle of the picture = width/2
-        for(int j = 0; j < width/2; j++)
-        {   //debug wants "RGBTRIPLE" x = the image -> swapping image to pixel change -> pixel change = x!!
-            RGBTRIPLE reflection = image[i][j];         //temporary = original
-            image[i][j] = image[i][width - 1 - j];      //original = creation
-            image[i][width - 1 - j] = reflection;       //creation = temporary
+    for (int i = 0; i < height; i++)
+    {   // swap in the middle of the picture = width/2
+        for (int j = 0; j < width / 2; j++)
+        {   // debug wants "RGBTRIPLE" x = the image -> swapping image to pixel change -> pixel change = x!!
+            RGBTRIPLE reflection = image[i][j];         // temporary = original
+            image[i][j] = image[i][width - 1 - j];      // original = creation
+            image[i][width - 1 - j] = reflection;       // creation = temporary
         }
     }
     return;
@@ -56,52 +54,51 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
-{   //build a temporary variable
+{   // build a temporary variable
     RGBTRIPLE temporary[height][width];
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
-        {   //temporary = original image
+        for (int j = 0; j < width; j++)
+        {   // temporary = original image
             temporary[i][j] = image[i][j];
         }
     }
     // build the 3x3 pixels around with the average tone of the RGB
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
-        {   //creation
+        for (int j = 0; j < width; j++)
+        {   // creation
             int totalRed = 0, totalGreen = 0, totalBlue = 0;
             float counter = 0.0;
 
-            for(int vertical = -1; vertical < 2; vertical++)
+            for (int vertical = -1; vertical < 2; vertical++)
             {
-                for(int horizontal = -1; horizontal < 2; horizontal++)
+                for (int horizontal = -1; horizontal < 2; horizontal++)
                 {
                     int madeVertical = i + vertical;
                     int madeHorizontal = j + horizontal;
-
-                    if(madeVertical < 0 || madeVertical > (height - 1) || madeHorizontal < 0 || madeHorizontal > (width - 1))
+                    
+                    if (madeVertical < 0 || madeVertical > (height - 1) || madeHorizontal < 0 || madeHorizontal > (width - 1))
                     {
                         continue;
                     }
-                totalRed += image[madeVertical][madeHorizontal].rgbtRed;
-                totalGreen += image[madeVertical][madeHorizontal].rgbtGreen;
-                totalBlue += image[madeVertical][madeHorizontal].rgbtBlue;
+                    totalRed += image[madeVertical][madeHorizontal].rgbtRed;
+                    totalGreen += image[madeVertical][madeHorizontal].rgbtGreen;
+                    totalBlue += image[madeVertical][madeHorizontal].rgbtBlue;
 
-                counter++;
-
+                    counter++;
                 }
             }
-            //math.h has the LL round function, to get to the nearest number
+            // math.h has the LL round function, to get to the nearest number
             temporary[i][j].rgbtRed = llround(totalRed / counter);
             temporary[i][j].rgbtGreen = llround(totalGreen / counter);
             temporary[i][j].rgbtBlue = llround(totalBlue / counter);
         }
     }
-    //put created blur effect into order in new .bmp
-    for(int i = 0; i < height; i++)
+    // put created blur effect into order in new .bmp
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             image[i][j] = temporary[i][j];
         }
