142cf8cea4226eb54bc71405f6d39a915a8aac92
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-25 11:43:23 +0000

    Fri, Aug 25, 2023, 12:43 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -45,9 +45,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for(int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j--)
+        for(int j = 0; j == width; j--)
         {
-            
+            image[i][j].rgbtRed;
+            image[i][j].rgbtGreen;
+            image[i][j].rgbtBlue;
         }
     }
     return;
