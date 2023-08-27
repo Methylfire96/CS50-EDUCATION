d97d8e96a8f79fdb09e706420edc44fd5f5e2a1d
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-27 13:27:24 +0000

    Sun, Aug 27, 2023, 2:27 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,9 +26,8 @@ int main(int argc, char *argv[])
     {   //if finding jpg file, the next file is 512byts far away
         if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff &(buffer[3] & 0xf0) == 0xe0)
         {
-            fclose(file);
-            fread(buffer, 1, 4, file);
-            printf("found jpg!! file\n");
+            sprintf(filename, "%03i.jpg", 2);
+            FILE *img = fopen(filename, "w");
         }
     }
     printf("found -- file\n");
