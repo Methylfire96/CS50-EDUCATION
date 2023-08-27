01c55020be0816f97a1a57f1f1b074f286fd1b07
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-27 14:37:24 +0000

    Sun, Aug 27, 2023, 3:37 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 const int BLOCK_SIZE = 512;
@@ -9,28 +9,28 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("wrong input! #err1\n");
-        return 1; //error1
+        return 1; // error1
     }
 
     char *file = argv[1];
     FILE *raw_file = fopen(file, "r");
-    //check if raw file = NULL, error2
+    // check if raw file = NULL, error2
     if (raw_file == NULL)
     {
         printf("No such file found. #err2\n");
         return 2;
     }
-    //if finding jpg file, the next file is 512byts far away
+    // if finding jpg file, the next file is 512byts far away
     uint8_t buffer[BLOCK_SIZE];
     // jpg first 4 inputs==signature = {0xff, 0xd8, 0xff, 0xe n, n = 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f};
     FILE *jpg = NULL;
     int counter = 0;
     while (fread(buffer, BLOCK_SIZE, 1, raw_file) == 1)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&(buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            //clode jpg file
-            if(jpg != NULL)
+            // close jpg file
+            if (jpg != NULL)
             {
                 fclose(jpg);
             }
