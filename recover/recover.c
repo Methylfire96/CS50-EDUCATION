5feae37dd5c28805a113a2b3ad7894f8dfcc3e7e
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-27 13:47:24 +0000

    Sun, Aug 27, 2023, 2:47 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,10 +23,9 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[BLOCKSIZE];
     // jpg first 4 inputs = {0xff, 0xd8, 0xff, 0xe n, n = 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f};
-    FILE *jpg
+    FILE *jpg = NULL;
     int counter = 0;
-    fread(buffer, 1, 4, file);
-    for (int i = 0; i < 4; i++)
+    while (fread(buffer, BLOCKSIZE, 1, file) == 1);
     {   //if finding jpg file, the next file is 512byts far away
         if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff &(buffer[3] & 0xf0) == 0xe0)
         {
