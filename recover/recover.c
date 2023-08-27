7f8513e7db8eface3f6548af52ebf862213978ed
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-27 14:07:25 +0000

    Sun, Aug 27, 2023, 3:07 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("wrong input! #err1\n");
-        return 1; //
+        return 1; //error1
     }
 
     string filename = argv[1];
