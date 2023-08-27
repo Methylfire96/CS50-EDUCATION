b7500744fe1c5edbce465250e9270ca035ec6bbe
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-27 13:57:24 +0000

    Sun, Aug 27, 2023, 2:57 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     // jpg first 4 inputs = {0xff, 0xd8, 0xff, 0xe n, n = 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f};
     FILE *jpg = NULL;
     int counter = 0;
-    while (fread(buffer, BLOCKSIZE, 1, file) == 1);
+    while (fread(buffer, BLOCKSIZE, 1, file) == 1)
     {   //if finding jpg file, the next file is 512byts far away
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&(buffer[3] & 0xf0) == 0xe0)
         {
@@ -36,13 +36,13 @@ int main(int argc, char *argv[])
             }
 
             //create new jpg file + counter (jpg1, jpg2,...)
-            string filename[8];
+
             sprintf(filename, "%03i.jpg", counter);
             jpg = fopen(filename, "w");
             if (jpg != NULL)
             {
                 fclose(file);
-                fprintf("No such file found. #err3\n");
+                printf("No such file found. #err3\n");
                 return 3;
             }
             counter++;
