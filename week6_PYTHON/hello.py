d27d6afaf33bb944a4dd2f7d5fe91851611751cb
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-29 20:42:44 +0000

    Tue, Aug 29, 2023, 9:42 PM +01:00

--- week6_PYTHON/hello.py
+++ week6_PYTHON/hello.py
@@ -3,4 +3,4 @@ from cs50 import get_string
 
 print("hello, world")
 answer = get_string("What is your name? ")
-print("hello,", answer)
+print("hello,", answer)  // also same: print(f"hello, {answer}")
