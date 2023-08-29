3d3e59fc9a959e36258121661851456c93d18d7c
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-29 21:54:45 +0000

    Tue, Aug 29, 2023, 10:54 PM +01:00

--- week6_PYTHON/agree.py
+++ week6_PYTHON/agree.py
@@ -1,6 +1,20 @@
+#s= input("Do u agree?")
+
+#if s == "Y" or s == "y": # also same if s in ["Y","y"]:
+    print("Agreed")
+#elif s == "N" or s == "n": # also same if s in ["N","n"]:
+    print("not agreed")
+
+
+
+
 s= input("Do u agree?")
 
-if s == "Y" or s == "y": # also same if s in ["Y","y"]:
+
+if s.lower() in ["y","yes"]
     print("Agreed")
-elif s == "N" or s == "n": # also same if s in ["N","n"]:
+elif s.lower() in ["N","n"]:
     print("not agreed")
+
+
+
