a5ef65d1eb4ec7fe3f0370ef2ff87ba5e23db290
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-30 16:30:10 +0000

    Wed, Aug 30, 2023, 5:30 PM +01:00

--- week6_PYTHON/moew.py
+++ week6_PYTHON/moew.py
@@ -1,4 +1,14 @@
-i = 0
-while i < 3:
+#i = 0
+#while i < 3:
+#    print("moew")
+#    i += 1
+
+
+def main():
+    for i in range(3):
+        moew()
+
+def moew():
     print("moew")
-    i += 1
+
+main()
