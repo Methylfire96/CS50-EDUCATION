f09cf38ee6365db114273e051a638426bab5ef8b
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-08-30 21:10:28 +0000

    Wed, Aug 30, 2023, 10:10 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -62,11 +62,11 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        new_person->parents[0] == NULL;
-        new_person->parents[1] == NULL;
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
         // TODO: Randomly assign alleles
-        new_person->alleles[0] = random_alleles();
-        new_person->alleles[1] = random_alleles();
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
 
     }
 
