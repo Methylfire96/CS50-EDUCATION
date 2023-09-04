20c2079af3b456eaab818285528706b56fd6a396
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-04 16:09:21 +0000

    Mon, Sep 4, 2023, 5:09 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,13 +1,11 @@
 // Implements a dictionary's functionality
-
+#include "dictionary.h"
 #include <ctype.h>
 #include <stdbool.h>
-#include <strings.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include "dictionary.h"
+#include <string.h>
+#include <strings.h>
 
 // Represents a node in a hash table
 typedef struct node
@@ -18,7 +16,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-int word_count = 0; //my counter in load
+int word_count = 0; // my counter in load
 const unsigned int N = 26;
 
 // Hash table
@@ -30,9 +28,9 @@ bool check(const char *word)
     // TODO
     // hash word to get index
     unsigned int index = hash(word);
-    //go through the list, until word is found(table[])
+    // go through the list, until word is found(table[])
     for (node *cursor = table[index]; cursor != NULL; cursor = cursor->next)
-    {       //compare word with current node word
+    {       // compare word with current node word
         if (strcasecmp(cursor->word, word) == 0)
         {
             return true;
@@ -47,7 +45,7 @@ unsigned int hash(const char *word)
     // TODO: Improve this hash function
     unsigned int hash = 0;
     // till end of word(\0)
-    for ( int i = 0; i < word[i] != '\0'; i++)
+    for (int i = 0; i < word[i] != '\0'; i++)
     {
         hash += toupper(word[i]) - 'A';
     }
@@ -58,15 +56,15 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    FILE * file = fopen(dictionary, "r");
+    FILE *file = fopen(dictionary, "r");
     if (file == NULL)
     {
         printf("cant open dict: %s\n", dictionary);
         return false;
     }
-    char word[LENGTH +1];
+    char word[LENGTH + 1];
     while (fscanf(file, "%s", word) != EOF)
-    {
+    {   //size of new, null = close file, otherwise copy, new to table - table new + word_count
         node *new_node = malloc(sizeof(node));
         if (new_node == NULL)
         {
@@ -85,7 +83,6 @@ bool load(const char *dictionary)
     fclose(file);
 
     return true;
-
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
