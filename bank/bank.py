a527bb06c73381411f74d7967a7068fdcf1ca278
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-05 16:06:40 +0000

    Tue, Sep 5, 2023, 5:06 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -3,9 +3,9 @@ from cs50 import get_string
 
 greetings = get_string("Greetings: ")
 
-if "H" in greetings[0]:
-    print("20$")
 if "hello" in greetings:
     print("0$")
+if "H" in greetings[0]:
+    print("20$")
 else:
     print("100$")
