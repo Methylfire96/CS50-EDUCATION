ef723ca28ef79d6ae1d2fe698ebc435f55a00d93
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-05 20:57:44 +0000

    Tue, Sep 5, 2023, 9:57 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,7 +5,7 @@ from cs50 import get_string
 figlet = Figlet()
 figlet.getFonts()
 try:
-    ask = get_string("what font do we use?: ")
+    ask = input("what font do we use?: ")
     figlet.setFont(font=ask)
     text = get_string("Which text do you want to put in a different font?: ")
     print(figlet.renderText(text))
