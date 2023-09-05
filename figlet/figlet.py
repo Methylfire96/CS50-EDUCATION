352131a807f03a380ff76b97d2f6b58c85f0e16c
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-05 18:00:05 +0000

    Tue, Sep 5, 2023, 7:00 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,9 +2,11 @@ from pyfiglet import Figlet
 from cs50 import get_string
 
 figlet = Figlet()
-
-
-ask = get_string("which font u wanna use?: ")
-figlet.setFont(font=ask)
-text = get_string("which text u wanna put in diff font?: ")
-print(figlet.renderText(text))
+figlet.getFonts()
+try:
+    ask = get_string("Which font do you want to use?: ")
+    figlet.setFont(font=ask)
+    text = get_string("Which text do you want to put in a different font?: ")
+    print(figlet.renderText(text))
+except Exception as e:
+    print("Error:", e)
