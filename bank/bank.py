a4267265e72ce8970119520318675ec7a5f612b8
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-05 16:19:30 +0000

    Tue, Sep 5, 2023, 5:19 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,11 +1,11 @@
-from sys import argv
+from sys import argv, string.isalpha
 from cs50 import get_string
 
 greetings = get_string("Greetings: ")
-if greetings = greetings.isalpha()
-if "hello" in greetings:
-    print("0$")
-if "H" or "h" in greetings[0]:
-    print("20$")
-else:
-    print("100$")
+if isalpha(greetings):
+    if "hello" in greetings:
+        print("0$")
+    if "H" or "h" in greetings[0]:
+        print("20$")
+    else:
+        print("100$")
