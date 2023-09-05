8b772482892f4ab5a021f9a904f6ac23282fb6ca
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-05 16:04:30 +0000

    Tue, Sep 5, 2023, 5:04 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,2 +1,11 @@
 from sys import argv
+from cs50 import get_string
 
+greetings = get_string("Greetings: ")
+
+if "H" in greetings[0]:
+    print("20$")
+if "hello" in greetings:
+    print("0$")
+else
+    print()
