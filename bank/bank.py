40bb2ec9b254757cf67e8d074860ae09c6fab39a
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-05 16:07:39 +0000

    Tue, Sep 5, 2023, 5:07 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -5,7 +5,7 @@ greetings = get_string("Greetings: ")
 
 if "hello" in greetings:
     print("0$")
-if "H" in greetings[0]:
+if "H" or "h" in greetings[0]:
     print("20$")
 else:
     print("100$")
