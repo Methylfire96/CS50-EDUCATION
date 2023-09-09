12bb7ef3d80107f1058d0b706944356b514cea3f
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-09 20:36:03 +0000

    Sat, Sep 9, 2023, 9:36 PM +01:00

--- week6_PYTHON/phonebook/phonebook.py
+++ week6_PYTHON/phonebook/phonebook.py
@@ -1,2 +1,10 @@
-from phonebook.csv
+import csv
+
+with open("phonebook.csv", "a") as file:
+
+    name = input("Name: ")
+    number = input("Number: ")
+
+    writer = csv.Dictwriter(file, fieldnames=["name", "number"])
+    writer.writerow([name, number])
 
