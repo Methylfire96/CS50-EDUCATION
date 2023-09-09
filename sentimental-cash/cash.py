157bfcea73eee5e60813b4b2edbb9d49115242f2
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-09 21:20:57 +0000

    Sat, Sep 9, 2023, 10:20 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,3 +1,8 @@
 # TODO
-int cents = input("How many cents do you get back?: ")
+cents = input("How many cents do you get back?: ")
+while cents < 0 or cents > 1000:
+    return cents
+
+else:
+    print("invalid input")
 
