9d09772730e1d0fe224befbaf33ca501f7836d1c
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-12 18:05:40 +0000

    Tue, Sep 12, 2023, 7:05 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -16,7 +16,11 @@ def main():
 def calc_index(text):
     letters = sum(c.isalpha() for c in text)
     words = len(text.split())
-    sentences = text.count('.') + text.count('!') + text.count('?'))
-
+    sentences = text.count('.') + text.count('!') + text.count('?')
+    L = (letters/words) * 100
+    S = (sentences/ words) * 100
+    index = 0.0588 * L - 0.296 * S - 15.8
+    return round(index)
 
 def get_grade(index):
+    if index 
