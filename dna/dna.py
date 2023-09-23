c7533162771e842c88c244dfa1ea68a01b82c713
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-09-23 23:53:07 +0000

    Sun, Sep 24, 2023, 12:53 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,8 +10,8 @@ def main():
 
 
     # TODO: Read database file into a variable
-    databases = sys.argv[1]
-    sequences = sys.argv[2]
+    databases = "/." + sys.argv[1]
+    sequences = "/." + sys.argv[2]
     with open(databases, "r") as csv:
         reader = csv.DictReader(csv)
         human = list(reader)
