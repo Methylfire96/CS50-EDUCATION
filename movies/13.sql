3f68a06616de0da9ad46b993bffa8ddef13c48d1
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-01 23:15:21 +0000

    Mon, Oct 2, 2023, 12:15 AM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,9 +1,9 @@
-SELECT DISTINCT people.name FROM people
+SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.id IN (
     SELECT movies.id FROM movies
-    JOIN stars ON stars.person_id = movies.id
+    JOIN stars ON stars.movie_id = movies.id
     JOIN people ON stars.person_id = people.id
     WHERE people.name = "Kevin Bacon" AND people.birth = 1958
 );
