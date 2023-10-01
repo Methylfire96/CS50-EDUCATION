a69fe53b538911400e7bd826209867e8c964ad34
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-01 00:47:09 +0000

    Sun, Oct 1, 2023, 1:47 AM +01:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,15 +1,5 @@
 SELECT DISTINCT people.name FROM people
 JOIN directors ON people.id = directors.person_id
-WHERE ratings.rating = "9.0";
-
-
-
-
-
-
-
-
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = "2004" AND people.birth IS NOT NULL
-ORDER BY people.birth;
+JOIN movies ON directors.movie_id = movies.id
+JOIN ratings ON movie.id = ratings.movie_id
+WHERE ratings.rating >= "9.0";
