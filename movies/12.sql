aae73cb66e127218494378910fdac496faa9ff06
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-01 22:51:47 +0000

    Sun, Oct 1, 2023, 11:51 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,11 +1,11 @@
 SELECT title FROM movies
 WHERE id IN (
     SELECT movie_id FROM stars
-    JOIN poeple ON stars.person_id = person.id
+    JOIN people ON stars.person_id = people.id
     WHERE people.name = "Bradley Cooper"
 )
 AND id IN (
     SELECT movie_id FROM stars
-    JOIN people ON stars.person_id = person.id
+    JOIN people ON stars.person_id = people.id
     WHERE people.name = "Jennifer Lawrence"
 );
