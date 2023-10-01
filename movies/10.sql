238bcf00782a3126248490846fe062a4993aba6b
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-01 00:39:29 +0000

    Sun, Oct 1, 2023, 1:39 AM +01:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,2 +1,14 @@
 SELECT DISTINCT people.name FROM people
-JOIN 
+JOIN directors ON people.id = 
+
+
+
+
+
+
+
+
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year = "2004" AND people.birth IS NOT NULL
+ORDER BY people.birth;
