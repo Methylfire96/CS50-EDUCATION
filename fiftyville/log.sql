0a64de20201d92b3670ab13dc4b5e9a258c89e32
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-02 18:58:36 +0000

    Mon, Oct 2, 2023, 7:58 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -46,14 +46,12 @@
     JOIN phone_calls ON people.phone_number = phone_calls.caller
     JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
     JOIN atm_transactions ON bank_accounts.person_id = atm_transactions.id
-    JOIN bank_accounts ON bank_accounts.person_id = people.id
-
     WHERE phone_calls.year = 2021 AND phone_calls.month = 7 AND phone_calls.day = 28 AND phone_calls.duration <=60
     AND bakery_security_logs.year = 2021 AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10
     AND atm_transactions.year = 2021 AND atm_transactions.month = 7 AND atm_transactions.day = 28 AND atm_transactions.atm_location = "Leggett Street" AND atm_transactions.transaction_type = "withdraw"
 
 
-
+JOIN bank_accounts ON bank_accounts.person_id = people.id
 
 
 
