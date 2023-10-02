6474fd2964d572b1e1f55d09155371d1c81c1913
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-02 17:38:37 +0000

    Mon, Oct 2, 2023, 6:38 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -36,4 +36,9 @@
     1106N58
 
 --search ATM withdwawal in the morning of the theft:
-    
+    SELECT * FROM atm_transactions
+    WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+
+
+    --------------
+    SELECT 
