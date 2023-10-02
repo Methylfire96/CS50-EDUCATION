1fafac373b9ff334732292b38182b876860e1364
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-02 19:56:24 +0000

    Mon, Oct 2, 2023, 8:56 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -78,7 +78,26 @@ city fiftyville: airport_name = Fiftyville Regional Airport
 
 
 --from the COMBINE from aboth, find those persons passport_number and compare it with the passenger list:
-    
+    SELECT DISTINCT passport_number FROM people
+    JOIN phone_calls ON poeple.phone_number = phone_calls.caller
+    JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+    JOIN atm_transactions ON atm_transactions.person_id = people.id
+    JOIN bank_accounts ON bank_accounts.person_id = people.id
+    WHERE pc.year = 2021
+  AND phone_calls.month = 7
+  AND phone_calls.day = 28
+  AND phone_calls.duration <= 60
+  AND bakery_security_logs.hour = 10
+  AND EXISTS (
+    SELECT 1
+    FROM passengers
+    JOIN flights ON passengers.flight_id = flights.id
+    WHERE flights.origin_airport_id = 8
+      AND flights.destination_airport_id = 4
+      AND flights.hour = 8
+      AND flights.minute = 20
+      AND passengers.passport_number = people.passport_number
+  );
 
 
 
