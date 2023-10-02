c81f0fbadc2c9ccb4daeeb047e77f12fb4802a93
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-02 14:31:18 +0000

    Mon, Oct 2, 2023, 3:31 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+status quo: theft took place on July 28, 2021 on "Humphrey Street"
+finding the description of that data above:
+    SELECT description FROM crime_scene_reports
+    WHERE month = 7 AND day = 28 AND street = "Humphrey Street";
+
