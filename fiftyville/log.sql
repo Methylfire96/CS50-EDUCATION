fa38c7d820ec984c4a817cb36b2f0861e4ec8693
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-02 14:45:26 +0000

    Mon, Oct 2, 2023, 3:45 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,5 +7,7 @@
     WHERE month = 7 AND day = 28 AND street = "Humphrey Street";
 --> theft took place at 10:15am in the Humphrey Street bakery ( 3 witnesse), littering at 16:36 (no witnesses)
 
---finding the bakery sec logs:
-    SELECT 
+--search the bakery sec logs:
+    SELECT license_plate FROM bakery_security_logs
+    WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
+--> there is one licenxe plate exit 1minute after the theft (5P2BI95)
