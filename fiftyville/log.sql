29f2a623f0b94121b6ce7342fbaf45f3620d2309
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-02 14:35:07 +0000

    Mon, Oct 2, 2023, 3:35 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-status quo: theft took place on July 28, 2021 on "Humphrey Street"
-finding the description of that data above:
+--status quo: theft took place on July 28, 2021 on "Humphrey Street"
+
+--finding the description of that data above:
     SELECT description FROM crime_scene_reports
     WHERE month = 7 AND day = 28 AND street = "Humphrey Street";
+--> theft took place at 10:15am in the Humphrey Street bakery ( 3 witnesse), littering at 16:36 (no witnesses)
 
+--finding the bakery sec logs:
+    SELECT 
