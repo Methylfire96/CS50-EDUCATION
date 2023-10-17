a7e106a17b501a5ace45fcbe938531a99ed79ba2
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-10-17 19:12:44 +0000

    Tue, Oct 17, 2023, 8:12 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -124,7 +124,7 @@ def register():
 
         hashed_password = generate_password_hash(password)
 
-        register= db.execute("INSERT INTO users WHERE username = ?, hash = ?)", (username, hashed_password))
+        register= db.execute(("INSERT INTO users WHERE username = ?, hash = ?"), (username, hashed_password))
 
 
         return redirect("/login", register=users)
