9b96d8d8f92ad359f77b293dbb2a485ca88674e8
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-11-03 21:39:33 +0000

    Fri, Nov 3, 2023, 9:39 PM +00:00

--- final_project/main.py
+++ final_project/main.py
@@ -1,30 +1,62 @@
-import pygame, sys
-from editor import Editor
+import pygame, sys, time
+
+# name
+pygame.display.set_caption("Final Project")
+
+# window
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-class Main:
-    def __init__(self):
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.clock = pygame.time.Clock()
+# player
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_MOVE = 5
+PLAYER_JUMP = 3
 
-        self.editor = Editor()
 
-    def run(self):
-        while True:
-            dt = self.clock.tick() /1000
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+# draw/ show
+def update(player):
+    WINDOW.fill("#6185f8")
+    pygame.draw.rect(WINDOW, "red", player)
 
-            self.editor.run(dt)
+    # always lost call of def update()
+    pygame.display.update()
 
-            pygame.display.update()
+def main():
+    run = True
+
+    player = pygame.Rect(400, WINDOW_HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        # player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player.x -= PLAYER_MOVE
+
+        if keys[pygame.K_d]:
+            player.x += PLAYER_MOVE
+
+        if keys[pygame.K_SPACE]:
+            player.y -= PLAYER_JUMP
+
+
+        # update the def update while run
+        update(player)
+
+    pygame.quit()
+
+
+
 
 if __name__ == "__main__":
-    main = Main()
-    main.run()
+    main()
+
 
 
