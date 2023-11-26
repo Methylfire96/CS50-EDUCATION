dde2b282762a39f9d86339f9943aa1b255f2df7b
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-11-26 22:11:03 +0000

    Sun, Nov 26, 2023, 10:11 PM +00:00

--- cs50_python/tip/tip.py
+++ cs50_python/tip/tip.py
@@ -1,22 +1,21 @@
-
-
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
+
 def dollars_to_float(d):
-    try:
-        return float(d[1:])
-    except ValueError:
-        return 0.00
+
+    d = d.replace("$", "")
+    return float(d)
 
 
 def percent_to_float(p):
-    try:
-        return float(p[1:]) / 100.0
-    except ValueError:
-        return 0.00
+
+    d = d.replace("%", "")
+    return float(p) / 100.0
+
+
 main()
 
