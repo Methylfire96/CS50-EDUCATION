136349941e473a8e7cabd770724a2f4c0656e318
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-11-26 21:09:48 +0000

    Sun, Nov 26, 2023, 9:09 PM +00:00

--- cs50_python/faces/faces.py
+++ cs50_python/faces/faces.py
@@ -1,25 +1,7 @@
 
-
-def convert(text):
-    if ":)" in text and ":(" in text:
-        text.replace(":)", "🙂")
-        text.replace(":(", "🙁")
-        return text
-    elif ":)" in text:
-        text.replace(":)", "🙂")
-    elif ":(" in text:
-        text.replace(":(", "🙁")
-    else:
-        return text
+def convert(n):
+    return(n.replace(":)", 🙂) and n.replace(":(", 🙁))
 
 
-
-
-def main():
-    text = input("How are you? ")
-    faces = convert(text)
-    print(faces)
-
-
-
-main()
+text = input ("How are you? ")
+convert(text)
