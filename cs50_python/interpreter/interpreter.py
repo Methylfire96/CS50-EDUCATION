501e1084e40d33f5daa642a91467b54f53813f15
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-11-28 13:40:47 +0000

    Tue, Nov 28, 2023, 1:40 PM +00:00

--- cs50_python/interpreter/interpreter.py
+++ cs50_python/interpreter/interpreter.py
@@ -11,14 +11,23 @@ def div(x, y):
     else:
         return "Cant divide through zero!"
 
+val1 = float(input("Value one: "))
+operator = input("operatior (+ , - , * , / ): ")
+val2 = float(input("Value two: "))
 
+if operator == "+":
+    result = add(val1, val2)
 
+if operator == "-":
+    result = sub(val1, val2)
 
+if operator == "*":
+    result = mult(val1, val2)
 
+if operator == "/":
+    result = div(val1, val2)
 
-x = input("Value one: ")
-y = input("operatior: ")
-z = input("Value two: ")
+else:
+    result = "wrong operator"
 
-result = x.strip() y z.strip()
-print(result)
+print(f"{result}")
