6ab94597ada0a5717a9bfbd7cbf1c8092c5bda73
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-11-28 19:37:35 +0000

    Tue, Nov 28, 2023, 7:37 PM +00:00

--- cs50_python/meal/meal.py
+++ cs50_python/meal/meal.py
@@ -1,9 +1,20 @@
 def main():
-    
-
+    time = input("What time is it? ")
+    result = convert(time)
 
 def convert(time):
+    conv = float(time)
 
+    if 7.0 <= time <= 8.0:
+        return "breakfast time"
+
+    if 12.0 <= time <= 13.0:
+        return "lunch time"
+
+    if 18.0 <= time <= 19.0:
+        return "dinner time"
+
+    else:
 
 
 if __name__ == "__main__":
