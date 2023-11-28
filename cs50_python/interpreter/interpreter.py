887e228c3d7e49454b657f58463ad738e171a42f
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-11-28 13:31:47 +0000

    Tue, Nov 28, 2023, 1:31 PM +00:00

--- cs50_python/interpreter/interpreter.py
+++ cs50_python/interpreter/interpreter.py
@@ -1,4 +1,21 @@
 
+def add(x, y):
+    return x + y
+def sub(x, y):
+    return x - y
+def mult(x, y):
+    return x * y
+def div(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cant divide through zero!"
+
+
+
+
+
+
 x = input("Value one: ")
 y = input("operatior: ")
 z = input("Value two: ")
