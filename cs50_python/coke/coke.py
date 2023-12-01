fcedef15332730315d70371106736b2c8c374f73
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-01 15:47:12 +0000

    Fri, Dec 1, 2023, 3:47 PM +00:00

--- cs50_python/coke/coke.py
+++ cs50_python/coke/coke.py
@@ -2,7 +2,7 @@ def main():
     amount_due = 50
     print(f"Amount Due: {amount_due}")
 
-    while amount_due > 0:
+    while amount_due >= 0:
         inp = int(input("Insert Coin: "))
         amount_due = insert(inp, amount_due)
         print(f"Amount Due: {amount_due}")
