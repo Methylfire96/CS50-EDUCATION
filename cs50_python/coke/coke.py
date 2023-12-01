b01335b5925d71bed1869efddccd72bda958c5a6
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-01 15:15:26 +0000

    Fri, Dec 1, 2023, 3:15 PM +00:00

--- cs50_python/coke/coke.py
+++ cs50_python/coke/coke.py
@@ -1,6 +1,6 @@
 def main():
     print("Amount Due: 50")
-    inp = input("Insert Coin: ")
+    inp = int(input("Insert Coin: "))
     calc = insert(inp)
     print(f"{calc}")
 
@@ -9,7 +9,7 @@ def main():
 
 
 def insert(c):
-    coins = {25, 10 ,5}
+    coins = [25, 10 ,5]
     due = 50
     if coins in due:
         result = due - coins
