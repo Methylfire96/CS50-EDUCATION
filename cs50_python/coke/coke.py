da51469e32d96b6e0c54a209fb95681cedfc7357
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-01 16:10:00 +0000

    Fri, Dec 1, 2023, 4:10 PM +00:00

--- cs50_python/coke/coke.py
+++ cs50_python/coke/coke.py
@@ -5,11 +5,10 @@ def main():
     while amount_due > 0:
         inp = int(input("Insert Coin: "))
         amount_due = insert(inp, amount_due)
-        if amount_due <= 0:
-            print("Change Owed: 0")
-        else:
-            print(f"Amount Due: {amount_due}")
+        print(f"Amount Due: {amount_due}")
 
+        if amount_due < 0:
+            print(f"Change Owed: {amount_due}")
 
 
 
@@ -20,7 +19,7 @@ def insert(coin, amount_due):
     if coin in coins:
         amount_due -= coin
         if amount_due < 0:
-            return amount_due + coin
+            return -amount_due
     else:
         print("Invalid coin (25, 10, 5)")
 
