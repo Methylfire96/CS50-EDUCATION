d00e523d62e844097038f86f21997648336a6b7a
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-01 15:41:21 +0000

    Fri, Dec 1, 2023, 3:41 PM +00:00

--- cs50_python/coke/coke.py
+++ cs50_python/coke/coke.py
@@ -1,23 +1,26 @@
 def main():
-    print("Amount Due: 50")
-    inp = int(input("Insert Coin: "))
-    calc = insert(inp)
-    print(f"Amount Due: {calc}")
+    amount_due = 50
+    print(f"Amount Due: {amount_due}")
+
+    while amount_due > 0:
+        inp = int(input("Insert Coin: "))
+        amount_due = insert(inp, amount_due)
+        print(f"Amount Due: {amount_due}")
+
+    else:
+        print("Change Oed: 0")
 
 
 
-
-
-def insert(coin):
+def insert(coin, amount_due):
     coins = {25, 10, 5}
-    due = 50
 
     if coin in coins:
-        result = due - coin
-        return result
+        amount_due -= coin
     else:
         print("Invalid coin (25, 10, 5)")
-        return due
+
+    return amount_due
 
 
 main()
