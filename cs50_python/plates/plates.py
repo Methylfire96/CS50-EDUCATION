d32787c4546ae261a9ab679bf80d029396cb0b12
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-02 13:36:33 +0000

    Sat, Dec 2, 2023, 1:36 PM +00:00

--- cs50_python/plates/plates.py
+++ cs50_python/plates/plates.py
@@ -6,12 +6,12 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    alphabet = [A,Z]
+    alphabet = [A:Z]
     numbers = [0,1,2,3,4,5,6,7,8,9]
     for c in s:
-        if s[0] and s[1] == alphabet:
+        if s[0:2] == alphabet:
             return True
-        elif s[-1] and s[-2] == numbers:
+        elif s[-1:-2] == numbers:
             return True
 
 
