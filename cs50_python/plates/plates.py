b517024e79906f1a34b3d17f82b85182f59f1929
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-02 14:36:22 +0000

    Sat, Dec 2, 2023, 2:36 PM +00:00

--- cs50_python/plates/plates.py
+++ cs50_python/plates/plates.py
@@ -9,15 +9,18 @@ def is_valid(s):
 
     if s[2:].startswith("0"):
         return False
+
     if not s[0:2].isalpha() or not s[0:2].isupper():
         return False
+
     if not 2 <= len(s) <= 6:
         return False
+
     if any(c.isdigit() for c in s[2:-1]):
         return False
+
     if any(not (c.isdigit() or c.isalpha()) for c in s):
         return False
-
 
     return True
 
