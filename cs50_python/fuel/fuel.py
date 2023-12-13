708b45fcfbf7abfe58781195fae32c14533533a5
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-13 21:04:55 +0000

    Wed, Dec 13, 2023, 9:04 PM +00:00

--- cs50_python/fuel/fuel.py
+++ cs50_python/fuel/fuel.py
@@ -1,12 +1,15 @@
 def main():
-    x = int(input("x: "))
-    y = int(input("y: "))
-    print(f"Fraction: {x}/{y}")
-    fraction = (x / y)
-    result = trans(fraction)
-    print(result)
+    while x, y > 0:
+        x = int(input("x: "))
+        y = int(input("y: "))
+        print(f"Fraction: {x}/{y}")
+        fraction = (x / y)
+        result = trans(fraction)
+        print(result)
+    else:
+        return False
 
 def trans(s):
-
+    
 
 main()
