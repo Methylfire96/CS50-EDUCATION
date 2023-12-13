5f57a407d5dcdf9e94834ecb91176231d60361a4
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-13 21:13:34 +0000

    Wed, Dec 13, 2023, 9:13 PM +00:00

--- cs50_python/fuel/fuel.py
+++ cs50_python/fuel/fuel.py
@@ -3,12 +3,13 @@ def main():
         x = int(input("x: "))
         y = int(input("y: "))
         print(f"Fraction: {x}/{y}")
-        fraction = (x / y)
-        result = trans(fraction)
-        print(result)
 
     except(ValueError, ZeroDivisionError):
         return False
+
+    fraction = (x / y)
+    result = trans(fraction)
+    print(result)
 
 def trans(fraction):
     if fraction <= 0.1:
