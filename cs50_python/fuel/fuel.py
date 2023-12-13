ba1a96c4cb6c86850e340a47804a5a5bb06f2f3a
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-13 21:50:22 +0000

    Wed, Dec 13, 2023, 9:50 PM +00:00

--- cs50_python/fuel/fuel.py
+++ cs50_python/fuel/fuel.py
@@ -1,27 +1,37 @@
-def main():
-    try:
-        x = int(input("x: "))
-        y = int(input("y: "))
-        print(f"Fraction: {x}/{y}")
-    fraction = (x / y)
-    result = trans(fraction)
-    print(result)
+def calculate_percentage(fraction):
+    while True:
+        try:
+            x, y = map(int, fraction.split('/'))
+
+            if x <= 0 or y <= 0 or x > y:
+                raise ValueError("Invalid fraction")
+
+            percentage = (x / y) * 100
+
+            if percentage <= 1:
+                return "E"
+            elif percentage >= 99:
+                return "F"
+            else:
+                return round(percentage)
+
+        except ZeroDivisionError:
+            print("Error: Division by zero. Please enter a valid fraction.")
+            fraction = input("Fraction: ")
+
+        except ValueError:
+            print("Error: Invalid input. Please enter a valid fraction.")
+            fraction = input("Fraction: ")
+
+if __name__ == "__main__":
 
 
+    while True:
+        fraction_input = input("Fraction: ")
 
-    except(ValueError, ZeroDivisionError):
-        return False
+        result = calculate_percentage(fraction_input)
 
-def trans(fraction):
-    if fraction <= 0.1:
-        return ("E")
-    elif fraction <= 0.25:
-        return ("25%")
-    elif fraction <= 0.50:
-        return ("50%")
-    elif fraction <= 0.75:
-        return("75%")
-    else:
-        return("F")
-
-main()
+        if isinstance(result, int):
+            print(f"{result}%")
+        else:
+            print(result)
