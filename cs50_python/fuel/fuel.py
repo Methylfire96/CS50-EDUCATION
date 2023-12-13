011a8bc45454669fd5974800730eed02198c137e
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-13 21:01:38 +0000

    Wed, Dec 13, 2023, 9:01 PM +00:00

--- cs50_python/fuel/fuel.py
+++ cs50_python/fuel/fuel.py
@@ -1,5 +1,10 @@
 def main():
-    
+    x = int(input("x: "))
+    y = int(input("y: "))
+    print(f"Fraction: {x}/{y}")
+    fraction = (x / y)
+    result = trans(fraction)
+    print(result)
 
 def trans(s):
 
