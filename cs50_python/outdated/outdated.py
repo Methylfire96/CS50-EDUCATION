4326171bff1a6a1e7d8c7b95a565036c06dca902
Methylfire96 <124165954+Methylfire96@users.noreply.github.com>
2023-12-17 21:06:07 +0000

    Sun, Dec 17, 2023, 9:06 PM +00:00

--- cs50_python/outdated/outdated.py
+++ cs50_python/outdated/outdated.py
@@ -1,9 +1,32 @@
 def main():
-    
+
+list = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+    while True:
+        try:
+            date = input("Date: ").split()
+            if date in list:
+                return switch(date)
+
+        except ValueError:
+            print("input is not an integar")
 
 
 def switch():
-
+    
 
 
 main()
